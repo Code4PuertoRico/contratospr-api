3aa0eea0b7a470670740fce873733f983270b6c4
José Padilla <jpadilla@webapplicate.com>
2019-02-19 20:09:14 -0500

    Remove non-api urls

--- contratospr/urls.py
+++ contratospr/urls.py
@@ -3,32 +3,14 @@ from django.contrib import admin
 from django.urls import include, path
 
 from .api import urls as api_urls
-from .contracts.views import (
-    contract,
-    contractor,
-    contractors,
-    entities,
-    entity,
-    filepreviews_webhook,
-    index,
-    search,
-    trends,
-)
+from .contracts.views import filepreviews_webhook
 from .utils.views import liveness, readiness
 
 urlpatterns = [
     path("__debug__/", include(debug_toolbar.urls)),
     path("admin/", admin.site.urls),
-    path("api/", include(api_urls)),
-    path("api/webhooks/filepreviews/", filepreviews_webhook),
+    path("webhooks/filepreviews/", filepreviews_webhook),
     path("health/liveness/", liveness),
     path("health/readiness/", readiness),
-    path("", index, name="index"),
-    path("buscar/", search, name="search"),
-    path("entidades/", entities, name="entities"),
-    path("entidades/<slug:entity_slug>/", entity, name="entity"),
-    path("contratos/<slug:contract_slug>/", contract, name="contract"),
-    path("contratistas/", contractors, name="contractors"),
-    path("contratistas/<slug:contractor_slug>/", contractor, name="contractor"),
-    path("trends/", trends, name="trends"),
+    path("", include(api_urls)),
 ]
