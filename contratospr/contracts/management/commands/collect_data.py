a860474f5951bd8d1e114aa2da5865869f4a4efb
José Padilla <jpadilla@webapplicate.com>
2020-12-23 11:56:15 -0500

    Use celery for periodic tasks

--- contratospr/contracts/management/commands/collect_data.py
+++ contratospr/contracts/management/commands/collect_data.py
@@ -1,9 +1,6 @@
-from datetime import date, datetime
-
-from dateutil.relativedelta import relativedelta
 from django.core.management.base import BaseCommand
 
-from ...models import CollectionJob
+from ...tasks import collect_data
 
 
 class Command(BaseCommand):
@@ -14,27 +11,7 @@ class Command(BaseCommand):
         parser.add_argument("--date-of-grant-end", nargs="?", type=str, default=None)
 
     def handle(self, *args, **options):
-        now = datetime.utcnow()
-
-        if options["date_of_grant_start"]:
-            date_of_grant_start = date.fromisoformat(options["date_of_grant_start"])
-        else:
-            date_of_grant_start = now + relativedelta(
-                months=-1, day=1, hour=0, minute=0, second=0, microsecond=0
-            )
-
-        if options["date_of_grant_end"]:
-            date_of_grant_end = date.fromisoformat(options["date_of_grant_end"])
-        else:
-            date_of_grant_end = (
-                now
-                + relativedelta(
-                    months=-1, day=1, hour=0, minute=0, second=0, microsecond=0
-                )
-                + relativedelta(months=+1, days=-1)
-            )
-
-        job = CollectionJob.objects.create(
-            date_of_grant_start=date_of_grant_start, date_of_grant_end=date_of_grant_end
+        collect_data.delay(
+            date_of_grant_start=options["date_of_grant_start"],
+            date_of_grant_end=options["date_of_grant_end"],
         )
-        job.process()
