1a4e9cf15ef92f1c0fb0debb6e3ab27c9a702a18
José Padilla <jpadilla@users.noreply.github.com>
2021-12-11 17:27:30 -0500

    Keep track of last processed contract

--- contratospr/contracts/management/commands/update_contracts.py
+++ contratospr/contracts/management/commands/update_contracts.py
@@ -1,3 +1,4 @@
+from django.core.cache import cache
 from django.core.management.base import BaseCommand
 
 from ...models import Contract
@@ -8,21 +9,47 @@ class Command(BaseCommand):
     help = "Update local contracts with data from remote source"
 
     def add_arguments(self, parser):
-        parser.add_argument("--limit", nargs="?", type=int, default=None)
-        parser.add_argument("--offset", nargs="?", type=int, default=None)
+        parser.add_argument("--limit", nargs="?", type=int, default=1000)
 
     def handle(self, *args, **options):
-        limit = options.pop("limit", 1000)
-        offset = options.pop("offset", 0)
+        limit = options.get("limit")
+
         contracts = (
             Contract.objects.select_related("entity")
             .all()
-            .order_by("-date_of_grant")
-            .only("pk", "number", "entity")[offset : offset + limit]
+            .order_by("pk")
+            .only("pk", "number", "entity")
         )
 
-        for contract in contracts.iterator():
-            self.stdout.write(f"=> Scraping contract {contract.pk} / {contract.number}")
-            scrape_contracts.delay(
-                contract_number=contract.number, entity_id=contract.entity.source_id
-            )
+        cur_offset = 0
+        cur_limit = 1
+
+        cache_key = "cmd:update_contracts:last_preview_id:limit={}".format(limit)
+        last_contract_id = cache.get(cache_key)
+
+        if last_contract_id:
+            self.stdout.write("=> Starting after {}".format(last_contract_id))
+            contracts = contracts.filter(pk__gt=last_contract_id)
+
+        while cur_limit <= limit:
+            cur_c = list(contracts[cur_offset:cur_limit].iterator())
+
+            if len(cur_c) > 0:
+                c = cur_c[0]
+                last_contract_id = c.pk
+
+                self.stdout.write(f"=> Scraping contract {c.pk} / {c.number}")
+                scrape_contracts(contract_number=c.number, entity_id=c.entity.source_id)
+
+                self.stdout.write("=> Last contract {}".format(last_contract_id))
+                cache.set(cache_key, last_contract_id)
+            else:
+                self.stdout.write(
+                    "=> Nothing found offset={} / limit={} / last_contract_id={}".format(
+                        cur_offset, cur_limit, last_contract_id
+                    )
+                )
+                break
+
+            cur_offset += 1
+            cur_limit += 1
